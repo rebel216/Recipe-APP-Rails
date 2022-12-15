139ce61cc3d13eb9be0a4cae7104722ed037330c
Ogiogio Victor <ogiogiovictor@gmail.com>
2022-12-15 08:55:52 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
- devise_for :users
- 
+devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "foods#index"
-  resources :users do
-    resources :foods, only: [:index, :show, :new, :create, :destroy]
-    resources :recipes, only: [:index, :show, :new, :create, :destroy, :public] do
-      resources :recipe_foods, only: [:create, :new, :destroy]
-    end
-  end
-
-  # Defines the root path route ("/")
-  get 'public_recipes' => 'recipes#public_recipes'
-  resources :shoppings, only:[:index]
+  root to: 'users#index'
+  resources :recipes
 end
