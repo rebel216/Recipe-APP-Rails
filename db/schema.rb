f10fc62545afb219568d5d7c44c09ed3030b1a10
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-16 13:04:15 +0530

    Fixed header

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_235832) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -65,11 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_12_235832) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
- 
-
-  add_foreign_key 'foods', 'users'
-  add_foreign_key 'recipe_foods', 'foods'
-  add_foreign_key 'recipe_foods', 'recipes'
-  add_foreign_key 'recipes', 'users'
-
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipes", "users"
 end
